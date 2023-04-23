0ae95fedecdb04fcbb35b58e38589523f1fd60eb
Nguyễn Hải Long <longnh2003@gmail.com>
2023-04-23 13:52:46 +0700

    Update regression_models.py

--- regression_models.py
+++ regression_models.py
@@ -7,72 +7,88 @@ from sklearn.linear_model import *
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.ensemble import RandomForestRegressor
 
-data = pd.read_csv('dataset.csv', index_col=0)
-x = data.drop('price', axis = 1)
+# import the dataset
+data = pd.read_csv('dataset.csv')
+x = data.drop('price', axis=1)
 y = data['price']
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=28)
+# split the dataset in to training and testing set
+x_train, x_test, y_train, y_test = train_test_split(x, y, 
+                                                    test_size=0.2, 
+                                                    random_state=28)
 
 def ridge_regression(x_train, x_test, y_train, y_test, n):
     model = Ridge(alpha=n/10)
     
+    # train the model
     model.fit(x_train, y_train)
     
+    # calculate the MAPE
     y_pred = model.predict(x_test)
     y_pred_rounded = [round(result, 2) for result in y_pred]
     mape = float("{:.2f}".format(100 * mean_absolute_percentage_error(y_test, y_pred_rounded)))
     
-    return mape
+    return y_pred_rounded, mape
 
 def lasso_regresion(x_train, x_test, y_train, y_test, n):
     model = Lasso(alpha=n/10)
     
+    # train the model
     model.fit(x_train, y_train)
     
+    # calculate the MAPE
     y_pred = model.predict(x_test)
     y_pred_rounded = [round(result, 2) for result in y_pred]
     mape = float("{:.2f}".format(100 * mean_absolute_percentage_error(y_test, y_pred_rounded)))
     
-    return mape
+    return y_pred_rounded, mape
 
 def k_neightbor_regression_unweighted(x_train, x_test, y_train, y_test, n):
     model = KNeighborsRegressor(n_neighbors=n, weights='uniform')
     
+    # train the model
     model.fit(x_train, y_train)
     
+    # calculate the MAPE
     y_pred = model.predict(x_test)
     y_pred_rounded = [round(result, 2) for result in y_pred]
     mape = float("{:.2f}".format(100 * mean_absolute_percentage_error(y_test, y_pred_rounded)))
     
-    return mape
+    return y_pred_rounded, mape
 
 def k_neightbor_regression_weighted(x_train, x_test, y_train, y_test, n):
     model = KNeighborsRegressor(n_neighbors=n, weights='distance')
     
+    # train the model
     model.fit(x_train, y_train)
     
+    # calculate the MAPE
     y_pred = model.predict(x_test)
     y_pred_rounded = [round(result, 2) for result in y_pred]
     mape = float("{:.2f}".format(100 * mean_absolute_percentage_error(y_test, y_pred_rounded)))
     
-    return mape
+    return y_pred_rounded, mape
 
 def random_forest_regression(x_train, x_test, y_train, y_test, n):
     model = RandomForestRegressor(max_depth=n)
     
+    # train the model
     model.fit(x_train, y_train)
     
+    # calculate the MAPE
     y_pred = model.predict(x_test)
     y_pred_rounded = [round(result, 2) for result in y_pred]
     mape = float("{:.2f}".format(100 * mean_absolute_percentage_error(y_test, y_pred_rounded)))
     
-    return mape
+    return y_pred_rounded, mape
+
+# plot graphs
 
 def lr_graph():
     lr_result = []
     
     for i in range (21):
-        lr_result.append([i, ridge_regression(x_train, x_test, y_train, y_test, i), lasso_regresion(x_train, x_test, y_train, y_test, i)])
-
+        lr_result.append([i, ridge_regression(x_train, x_test, y_train, y_test, i)[1], lasso_regresion(x_train, x_test, y_train, y_test, i)[1]])
+    
     constant = [row[0] / 10 for row in lr_result]
     ridge = [row[1] for row in lr_result]
     lasso = [row[2] for row in lr_result]
@@ -89,12 +105,12 @@ def knr_graph():
     knr_result = []
     
     for i in range (1,51):
-        knr_result.append([i, k_neightbor_regression_unweighted(x_train, x_test, y_train, y_test, i), k_neightbor_regression_weighted(x_train, x_test, y_train, y_test, i)])
+        knr_result.append([i, k_neightbor_regression_unweighted(x_train, x_test, y_train, y_test, i)[1], k_neightbor_regression_weighted(x_train, x_test, y_train, y_test, i)[1]])
 
     k = [row[0] for row in knr_result]
     knru = [row[1] for row in knr_result]
     knrw = [row[2] for row in knr_result]
-
+    # plot
     plt.plot(k, knru, label='Uniform weight')
     plt.plot(k, knrw, label='Distance weight')
     plt.legend()
@@ -107,7 +123,7 @@ def rfr_graph():
     rfr_result = []
     
     for i in range (2,101):
-        rfr_result.append([i, random_forest_regression(x_train, x_test, y_train, y_test, i)])
+        rfr_result.append([i, random_forest_regression(x_train, x_test, y_train, y_test, i)[1]])
 
     max_depth = [row[0] for row in rfr_result]
     rfr = [row[1] for row in rfr_result]
