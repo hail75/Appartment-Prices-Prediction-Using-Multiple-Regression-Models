2bd1d86ae894cda5de33b6acfced858cda3ca2ef
Nguyễn Hải Long <longnh2003@gmail.com>
2023-06-24 11:02:00 +0700

    Update calculate_best_forest.py

--- calculate_best_forest.py
+++ calculate_best_forest.py
@@ -12,7 +12,7 @@ x = data.drop('price', axis=1)
 y = data['price']
 
 # Split the data into training and test sets
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42, shuffle=True)
 
 # Define the parameter grid for grid search
 param_grid = {
